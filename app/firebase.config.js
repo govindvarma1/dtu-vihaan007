ee4995774828cab30f3a2e27e7ccc0e1d5a55418
Sruthik Koneti <sruthikkoneti@gmail.com>
2024-04-12 10:14:08 +0530

    initial commit

---
+++ app/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBO_HPqhBOWRGVbmPgieo7kMIcvSqpk1bw",
+    authDomain: "dtu-vihaan007.firebaseapp.com",
+    projectId: "dtu-vihaan007",
+    storageBucket: "dtu-vihaan007.appspot.com",
+    messagingSenderId: "358480911006",
+    appId: "1:358480911006:web:7bf7b8eec4a6815fb8b98f",
+    measurementId: "G-G37X8MYMFJ"
+  };
---
+++ app/firebase.js
@@ -0,0 +1,8 @@
+import firebaseConfig from "./firebase.config";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
