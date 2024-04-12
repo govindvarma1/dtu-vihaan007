5e6f74eb3b6fcaad1a3c0f4070d1f11be7340e19
Govinda Varma <98372421+govindvarma1@users.noreply.github.com>
2024-04-12 11:18:55 +0530

    configured tailwind

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from "react";
 
-export default function Home() {
-  return (
-    <div>Home</div>
-  )
+export default function () {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        </div>
+    );
 }
