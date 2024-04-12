fdebe3fd17fbf116aa421bbe8fed19e41da690d8
Sruthik Koneti <sruthikkoneti@gmail.com>
2024-04-12 12:42:58 +0530

    bank announcement

--- backend/models/Bank.js
+++ backend/models/Bank.js
@@ -10,7 +10,35 @@ const BankSchema=new mongoose.Schema(
         password:{
             type:String,
             required:true
-        }
+        },
+        email:{
+            type:String,
+            required:true
+        },
+        phoneNumber:{
+            type:String,
+            required:true
+        },
+        address:{
+            type:String,
+            required:true
+        },
+        city:{
+            type:String,
+            required:true
+        },
+        state:{
+            type:String,
+            required:true
+        },
+        pincode:{
+            type:String,
+            required:true
+        },
+        posts:[{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "BankPost"
+        }]
     }
 )
 
--- backend/routes/bankRoutes.js
+++ backend/routes/bankRoutes.js
@@ -1,10 +1,12 @@
 import express from "express"
-import { bankSingUp } from "../controllers/bankController.js"
+import { announcePost, bankLogin, bankSingUp } from "../controllers/bankController.js"
+import { auth } from "../middleware/auth.js"
 
 const router = express.Router()
 
 router.post("/register",bankSingUp)
-
+router.post("/login",bankLogin)
+router.post("/announce",auth,announcePost)
 
 
 
